efdfcd3c7d5ac56abce7e953ef38580b171e18c2
Alleton <jfalleton@gmail.com>
2014-10-13 22:09:08 +0200

    Read Sudoku files

--- src/com/legallic/sudoku/Initializer.java
+++ src/com/legallic/sudoku/Initializer.java
@@ -1,33 +1,91 @@
 package com.legallic.sudoku;
 
 import com.legallic.sudoku.model.Cell;
+
 import com.legallic.sudoku.model.Matrix;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class Initializer {
 
-	public static Matrix initialize(int[][] sudoku){
+
+	public static Matrix initialize(String SudokuFile){
 		Matrix matrix = new Matrix();
 		
-		for(int rowIndex=0; rowIndex < matrix.getMatrixSize(); rowIndex++){
-			for(int colIndex=0; colIndex < matrix.getMatrixSize(); colIndex++){
-				Cell cell = new Cell();
-				cell.setColIndex(colIndex);
-				cell.setRowIndex(rowIndex);
-				int value = sudoku[rowIndex][colIndex];
-				cell.setDefinitiveValue(value);
-				if(value > 0){
-					cell.setFound(true);
+		 try {
+			 
+		    // open file input stream
+			 BufferedReader reader = new BufferedReader(new FileReader(SudokuFile));
+
+			// read file line by line
+			String line = null;
+			String splitBy = ",";  
+				
+			Scanner scanner = null;
+			int rowIndex = 0;
+			int colIndex = 0;
+					
+			while ((line = reader.readLine()) != null) {
+				char aChar = line.charAt(0);
+				if ( aChar != '#' ) {
+					
+				
+				// read line 
+				scanner = new Scanner(line);
+				scanner.useDelimiter(splitBy);
+				while (scanner.hasNext()) {
+					String strvalue = scanner.next() ;
+						
+
+						//int value = (Integer.parseInt(scanner.next()));
+						int value = (Integer.parseInt(strvalue )) ;
+						
+						Cell cell = new Cell();
+						cell.setColIndex(colIndex);
+						cell.setRowIndex(rowIndex);
+						
+						//int value = sudoku[rowIndex][colIndex];
+						
+						
+						cell.setDefinitiveValue(value);
+						if(value > 0){
+							cell.setFound(true);
+						}
+						boolean[] allIsPossible = new boolean[matrix.getMatrixSize() + 1];
+						// TODO : initialiser de meilleur façon
+						for (int i = 1; i < allIsPossible.length; i++) {
+							allIsPossible[i] = !cell.isFound();
+						}
+						cell.setPossibleValues(allIsPossible);
+						cell.setSquareIndex(colIndex / 3 + (rowIndex / 3 ) * 3);
+						matrix.getCells()[rowIndex][colIndex] = cell;
+						
+						colIndex++;
+					
+					
 				}
-				boolean[] allIsPossible = new boolean[matrix.getMatrixSize() + 1];
-				// TODO : initialiser de meilleur façon
-				for (int i = 1; i < allIsPossible.length; i++) {
-					allIsPossible[i] = !cell.isFound();
+					//empList.add(emp);
+				
+				colIndex = 0;
+				rowIndex++;
 				}
-				cell.setPossibleValues(allIsPossible);
-				cell.setSquareIndex(colIndex / 3 + (rowIndex / 3 ) * 3);
-				matrix.getCells()[rowIndex][colIndex] = cell;
 			}
-		}
+				//close reader
+		       
+			reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		         		
 		return matrix;
 	}
+	
+	
 }
--- src/com/legallic/sudoku/SudokuSolverMain.java
+++ src/com/legallic/sudoku/SudokuSolverMain.java
@@ -1,5 +1,7 @@
 package com.legallic.sudoku;
 
+
+
 import com.legallic.sudoku.exception.SudokuException;
 import com.legallic.sudoku.model.Matrix;
 import com.legallic.sudoku.service.Solver;
@@ -7,105 +9,35 @@ import com.legallic.sudoku.service.Solver;
 public class SudokuSolverMain {
 	
 	
-	private static int[][] toSolveFull = {
-			{1,9,2,8,3,7,4,6,5},
-			{3,7,5,4,5,9,1,8,2},
-			{4,8,6,5,2,1,7,3,9},
-			{5,3,9,6,1,4,2,7,8},
-			{2,4,1,9,7,8,6,5,3},
-			{8,6,7,2,5,3,9,1,4},
-			{6,2,8,1,4,5,3,9,7},
-			{9,1,3,7,8,2,5,4,6},
-			{7,5,4,3,9,6,8,2,1}
-	};
-	
-	private static int[][] toSolvePart = {
-		{1,9,0,0,3,7,4,6,5},
-		{3,7,5,4,5,9,1,8,2},
-		{4,8,6,5,2,1,7,3,9},
-		{5,3,9,6,1,4,2,7,8},
-		{2,4,1,9,7,8,6,5,3},
-		{8,6,7,2,5,3,9,1,4},
-		{6,2,8,1,4,5,3,9,7},
-		{9,1,3,7,8,2,5,4,6},
-		{7,5,4,3,9,6,8,2,0}
-	};	
-	
-	private static int[][] toto1 = {
-		{0,0,1,4,0,5,2,0,0},
-		{2,0,0,7,1,6,0,0,8},
-		{6,0,0,0,0,0,0,0,3},
-		{0,0,3,0,6,0,9,0,0},
-		{0,0,0,9,0,7,0,0,0},
-		{0,0,7,0,5,0,4,0,0},
-		{7,0,0,0,0,0,0,0,9},
-		{3,0,0,6,4,9,0,0,5},
-		{0,0,9,1,0,8,3,0,0}
-	};	
-	
-	private static int[][] lemondeDifficile = {
-		{1,9,0,0,3,0,0,0,5},
-		{0,0,5,0,0,9,0,0,2},
-		{0,0,0,0,0,1,7,3,0},
-		{0,0,0,0,0,4,0,7,8},
-		{0,0,1,9,0,8,6,0,0},
-		{8,6,0,2,0,0,0,0,0},
-		{0,2,8,1,0,0,0,0,0},
-		{9,0,0,7,0,0,5,0,0},
-		{7,0,0,0,9,0,0,2,1}
-	};	
-	
-	private static int[][] lemondeTresDifficile = {
-		{0,0,0,9,0,1,7,0,3},
-		{0,0,0,0,0,8,0,1,0},
-		{0,0,0,0,7,5,9,0,0},
-		{5,8,0,3,0,0,1,2,0},
-		{4,0,0,0,0,0,0,0,5},
-		{0,2,7,0,0,9,0,4,6},
-		{0,0,1,8,9,0,0,0,0},
-		{0,3,0,7,0,0,0,0,0},
-		{9,0,8,5,0,4,0,0,0}
-	};	
-	
-	private static int[][] lemondedudimanche = {
-		{0,9,3,0,0,0,0,0,7},
-		{0,6,0,3,2,0,0,8,0},
-		{8,2,0,7,0,0,0,1,5},
-		{0,0,0,2,0,0,0,0,0},
-		{0,8,0,4,3,1,0,5,0},
-		{0,0,0,0,0,6,0,0,0},
-		{2,5,0,0,0,3,0,4,6},
-		{0,1,0,0,4,8,0,3,0},
-		{3,0,0,0,0,0,5,7,0}
-	};		
-	
-	private static int[][] extreme = {
-		{3,0,0,0,1,0,0,0,9},
-		{0,8,0,7,0,4,0,1,0},
-		{0,0,5,0,0,0,4,0,0},
-		{0,3,0,6,0,2,0,4,0},
-		{6,0,0,0,0,0,0,0,3},
-		{0,7,0,9,0,3,0,5,0},
-		{0,0,3,0,0,0,5,0,0},
-		{0,2,0,1,0,7,0,8,0},
-		{8,0,0,0,2,0,0,0,6}
-	};		
-	
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		long startTime = System.currentTimeMillis();
-		Matrix matrix = Initializer.initialize(extreme);
+		
+		SudokuGetPropertyValues properties = new SudokuGetPropertyValues();		
+		System.out.println (properties.getPropValue("SudokuFile"));
+
+		String SudokuFile = properties.getPropValue("SudokuFile") ;
+		String SudokuFolder = properties.getPropValue("SudokuFolder") ;
+
+		
+		
+		Matrix matrix = Initializer.initialize(SudokuFolder + "/" + SudokuFile);
 		Solver solver = new Solver(matrix);
 		System.out.println("###############################################");
-		System.out.println("Tentative de résolution de la matrice suivante");
+		System.out.println("Tentative de resolution de la matrice suivante");
 		System.out.println("###############################################");
 		System.out.println(matrix);
+		
+		
+
 		try {
-			System.out.println("Ai-je réussi ? : " + solver.solve());
+			System.out.println("Ai-je reussi ? : " + solver.solve());
 			System.out.println(solver.getMatrix());
 			System.out.println(" En " + (System.currentTimeMillis() - startTime) + " ms !!!!!!!!!!!!! ");	
 		} catch (SudokuException e) {
 			System.err.println(e.getMessage());
 			e.printStackTrace();
 		}
+
+		
 	}
 }
